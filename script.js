ac3fe45c412db1932ae32d68e59fe4f249efbcb3
Ansel Zeng <ansel.zeng.yp@gmail.com>
2020-08-03 18:09:35 -0400

    Added runaway keyboard

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
 window.addEventListener('keydown', function(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-    if (!audio) return;
-    audio.currentTime = 0
-    audio.play()
-    key.classList.add('playing')
-})
-
-  function removeTransition(e){
-    if(e.propertyName !== 'transform') return
-    this.classList.remove('playing')
-}
-
-const keys = document.querySelectorAll('.key')
-keys.forEach(key => key.addEventListener('transitionend', removeTransition))
-
-/*
-window.addEventListener('keydown', function(e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-
+    
     if (!audio) return;
     audio.currentTime = 0
     audio.play()
@@ -32,5 +14,4 @@ function removeTransition(e){
 }
 
 const keys = document.querySelectorAll('.key')
-buttons.forEach(key => key.addEventListener('transitioned', removeTransition))
-*/
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
